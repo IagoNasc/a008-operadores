55195ae3fbef03172add1db6714ae481d72905e9
unknown <iagosto.nasc@gmail.com>
2022-08-30 14:43:45 -0300

    Commit A008-Operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let numero = prompt("Coloque um numero par")
+console.log(numero/=2,);
+
+//Ele divide o numero impar em numero quebrado
---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+let numero1 = prompt("Coloque um numero")
+console.log(numero1);
+
+let numero2 = prompt("Coloque um numero novamente")
+console.log(numero2);
+
+console.log("O primeiro numero é maior que o segundo?", numero1 > numero2);
+console.log("O primeiro numero é igual ao segundo?", numero1 == numero2);
+console.log("O primeiro numero é divisivel pelo segundo?", numero1%numero2 === 0 );
